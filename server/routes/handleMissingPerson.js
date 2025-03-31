76a43a77161184c37ccd985451168de1196411d3
linnkhant07 <lkty07@gmail.com>
2025-03-31 10:56:33 -0700

    Geocoding working - Post route

--- server/routes/handleMissingPerson.js
+++ server/routes/handleMissingPerson.js
@@ -18,17 +18,8 @@ router.get('/', async(req, res) => {
 });
 
 
-//GET, only for rendering (only not-found people)
-router.get('/notfound', async (req, res) => {
-  try {
-      const reports = await MissingPerson.find({ found: false });
-      res.status(200).json(reports);
-  } catch (e) {
-      res.status(500).json({ error: e.message });
-  }
-});
-
 // create route with geocoding functionality
+
 router.post('/', async (req, res) => {
     try {
       const { locationOfMissingPerson, ...rest } = req.body;
