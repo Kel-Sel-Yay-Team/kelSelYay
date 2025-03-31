ec84a01e5a576472107a1721747bdb7d0c26674d
linnkhant07 <lkty07@gmail.com>
2025-03-30 21:42:07 -0700

    MongoDB first test collection working

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import Test from './models/Test.js';
 
 dotenv.config();
 const app = express();
@@ -11,5 +12,15 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((err) => console.error('❌ MongoDB connection error:', err));
 
 app.get('/', (req, res) => res.send('API is working'));
+
+app.post('/test', async (req, res) => {
+  try {
+      const test = new Test({ message: req.body.message });
+      await test.save();
+      res.status(201).json({ success: true, data: test });
+  } catch (err) {
+      res.status(500).json({ success: false, error: err.message });
+  }
+});
 
 app.listen(3001, () => console.log('Backend running on http://localhost:3001'));
