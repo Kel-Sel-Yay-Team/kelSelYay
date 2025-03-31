b356ab0fe3d5cff27f7c3af21250aa9afddd9106
linnkhant07 <lkty07@gmail.com>
2025-03-31 10:56:33 -0700

    Geocoding working - Post route

--- server/src/models/MissingPerson.js
+++ server/src/models/MissingPerson.js
@@ -7,6 +7,8 @@ const MissingPersonSchema = new mongoose.Schema({
     missingPersonDescription: String,
     relationshipToReporter: String,
     locationOfMissingPerson: {type : String,  required: true},// From input box, e.g. "23 street, Yangon, Myanmar"
+    lat: { type: Number, required: true },  
+    lng: { type: Number, required: true },
     timeSinceMissing: Number, // in hours or days
     imageUrl: String, // image path or URL
     found: { type: Boolean, default: false },
