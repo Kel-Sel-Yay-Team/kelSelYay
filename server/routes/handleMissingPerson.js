1167e251a4ce48433c53b1c9917874c87e1d7480
linnkhant07 <lkty07@gmail.com>
2025-04-01 15:09:02 -0700

    render only notfound people + sighted people are taken off from map immediately

--- server/routes/handleMissingPerson.js
+++ server/routes/handleMissingPerson.js
@@ -17,10 +17,18 @@ router.get('/', async(req, res) => {
     }
 });
 
-// create route with geocoding functionality
+
+//GET, only for rendering (only not-found people)
+router.get('/notfound', async (req, res) => {
+  try {
+      const reports = await MissingPerson.find({ found: false });
+      res.status(200).json(reports);
+  } catch (e) {
+      res.status(500).json({ error: e.message });
+  }
+});
 
 // create route with geocoding functionality
-
 router.post('/', async (req, res) => {
     try {
       const { locationOfMissingPerson, ...rest } = req.body;
@@ -60,6 +68,7 @@ router.post('/', async (req, res) => {
       res.status(500).json({ error: err.message });
     }
   });
+
 
   //edit (with reporterName validation)
   router.put("/:id", async (req, res) => {
