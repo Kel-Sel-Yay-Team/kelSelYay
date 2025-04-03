56a7db35062b9806f31662f619401dd1ec9e2100
Hlaing, Khant Nyi <knyihlaing@go.pasadena.edu>
2025-04-03 10:58:42 -0700

    analytics

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { LanguageProvider } from "./context/LanguageContext";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -64,6 +65,7 @@ export default function RootLayout({ children }) {
         <LanguageProvider>
             {children}
         </LanguageProvider>
+        <GoogleAnalytics gaId="G-4QEMBKQBTJ"/>
       </body>
     </html>
   );
